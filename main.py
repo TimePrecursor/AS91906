a8e972fa5b0821371c5dc44d40c17928521da379
21006 <21006@fraser.school.nz>
2025-03-06 12:25:50 +1300

    Started Window Setup (added two temporarily empty functions) V 0.15

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
+from idlelib.zoomheight import set_window_geometry
+
 import PyQt5
 from PyQt5.QtCore import QMetaObject
 from PyQt6.QtCore import QCoreApplication
 import PyQt6
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
 
-class AnotherWindow(QWidget):
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
+from PyQt6.QtWidgets import *
+
+
+class Window(QMainWindow):
    """
    This "window" is a QWidget. If it has no parent, it
    will appear as a free-floating window as we want.
@@ -14,8 +19,8 @@ class AnotherWindow(QWidget):
       super().__init__()
       # setting title
       self.Currentuser = str()
-      self.setWindowTitle("Python ")
-
+      # self.setWindowTitle("Python ")
+      self.setFixedSize(500, 300)
       # setting geometry
       self.setGeometry(100, 100, 100, 100)
 
@@ -32,16 +37,25 @@ class AnotherWindow(QWidget):
 
 
    def UiComponents(self):
-      pass
+      self.TempUserInputBox = QLineEdit(self)
+      self.TempUserInputBox.setGeometry(100,100,100,100)
+      self.TempUserInputBox.show()
+
+
    def mainWindowSetup(self):
       pass
 
 
+def main():
+   # create pyqt5 app
+   App = QApplication(sys.argv)
+
+   # create the instance of our Window
+   window = Window()
+
+   # start the app
+   sys.exit(App.exec())
+
+
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-
-   window = QMainWindow()
-   window.show()
-
-   # Start the event loop.
-   app.exec()
+   main()
